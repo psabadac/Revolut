eec0fbb71029803450779baa35af2be86e77bdf8
Petru Sabadac <petru.sabadac@outlook.com>
2020-01-14 18:06:42 +0200

    Drafted recycle view layout

--- app/src/main/java/com/example/revolut/MainActivity.kt
+++ app/src/main/java/com/example/revolut/MainActivity.kt
@@ -2,11 +2,17 @@ package com.example.revolut
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        currency_title.text = "USD"
+        currency_description.text = "US Dollar"
+        country_flag.setBackgroundResource(R.drawable.us)
+        currency_amount.setText("1183.06")
     }
 }
